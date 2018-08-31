f311bcb5b4ee1513fef8bff0e0d0fe4ae3244e4a
Italo Costa Martins <i.martins@portrix.net>
2018-08-31 15:35:13 +0200

    Adjustments to Admin view and register view.

--- web/haw20y-client/src/app/admin/admin.component.ts
+++ web/haw20y-client/src/app/admin/admin.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+import {HawEvent} from '../models/hawevent';
 
 @Component({
   selector: 'app-admin',
@@ -9,15 +11,15 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class AdminComponent implements OnInit {
 
-  dataSource: any = [
-    {id: 0, name: 'Italo Costa Martins', country: {id: 0, name: 'Brazil'}},
-    {id: 1, name: 'Nizami Zamanov', country: {id: 1, name: 'Arzebajan'}},
-    {id: 2, name: 'Lorant Vrinceanu', country: {id: 2, name: 'Moldova'}},
-    {id: 3, name: 'Oleksi IDontCare', country: {id: 3, name: 'Oleksiland'}},
-    {id: 4, name: 'Saaker Whatever', country: {id: 4, name: 'Saakerland'}},
-    {id: 5, name: 'Leutlt Nope', country: {id: 5, name: 'Nopeland'}},
-    {id: 6, name: 'Dummy Dumber', country: {id: 6, name: 'Dumbland'}},
-  ];
+  eventForm: FormGroup;
+
+  displayedColumns = ['id', 'name', 'country', 'actions'];
+
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+
+  isNewEvent: Boolean = true;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   public events = [
     {
@@ -25,7 +27,7 @@ export class AdminComponent implements OnInit {
       dateCreated: new Date(),
       title: 'HAW 20 Years',
       dateOfEvent: new Date(),
-      location: {id: 0, name: 'Germany'},
+      location: {id: 1, name: 'Brazil'},
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
       'Suspendisse id iaculis purus, vel ullamcorper nisi. ' +
       'Donec hendrerit nisl eget neque laoreet mollis. ' +
@@ -40,7 +42,7 @@ export class AdminComponent implements OnInit {
       dateCreated: new Date(),
       title: 'HAW 20 Years',
       dateOfEvent: new Date(),
-      location: {id: 0, name: 'Germany'},
+      location: {id: 1, name: 'Brazil'},
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
       'Suspendisse id iaculis purus, vel ullamcorper nisi. ' +
       'Donec hendrerit nisl eget neque laoreet mollis. ' +
@@ -55,7 +57,7 @@ export class AdminComponent implements OnInit {
       dateCreated: new Date(),
       title: 'HAW 20 Years',
       dateOfEvent: new Date(),
-      location: {id: 0, name: 'Germany'},
+      location: {id: 1, name: 'Brazil'},
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
       'Suspendisse id iaculis purus, vel ullamcorper nisi. ' +
       'Donec hendrerit nisl eget neque laoreet mollis. ' +
@@ -70,7 +72,7 @@ export class AdminComponent implements OnInit {
       dateCreated: new Date(),
       title: 'HAW 20 Years',
       dateOfEvent: new Date(),
-      location: {id: 0, name: 'Germany'},
+      location: {id: 1, name: 'Brazil'},
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
       'Suspendisse id iaculis purus, vel ullamcorper nisi. ' +
       'Donec hendrerit nisl eget neque laoreet mollis. ' +
@@ -85,7 +87,7 @@ export class AdminComponent implements OnInit {
       dateCreated: new Date(),
       title: 'HAW 20 Years',
       dateOfEvent: new Date(),
-      location: {id: 0, name: 'Germany'},
+      location: {id: 1, name: 'Brazil'},
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
       'Suspendisse id iaculis purus, vel ullamcorper nisi. ' +
       'Donec hendrerit nisl eget neque laoreet mollis. ' +
@@ -101,10 +103,16 @@ export class AdminComponent implements OnInit {
     name: 'Oleksiland'
   }, {id: 5, name: 'Saakerland'}];
 
-  eventForm: FormGroup;
-
   constructor(private router: Router, private formBuilder: FormBuilder) {
-    this.createRegisterForm();
+    this.createEventForm();
+  }
+
+  /**
+    * Set the paginator after the view init since this component will
+    * be able to query its view for the initialized paginator.
+  */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit() {
@@ -118,7 +126,7 @@ export class AdminComponent implements OnInit {
     this.router.navigate(['home']);
   }
 
-  createRegisterForm() {
+  createEventForm() {
     this.eventForm = this.formBuilder.group({
       event: [''],
       eventTitle: [''],
@@ -128,4 +136,62 @@ export class AdminComponent implements OnInit {
     });
   }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  onDeleteUser(element: Element) {
+    console.log(element);
+  }
+
+  onSubmitEvent() {
+    console.log(this.eventForm);
+  }
+
+  onSearchUser() {
+  }
+
+  onEventSelect(event: HawEvent) {
+    if (typeof event !== 'undefined') {
+      this.isNewEvent = false;
+      this.eventForm.reset({
+        eventTitle: event.title,
+        eventLocation: event.location.id,
+        eventDate: event.dateOfEvent,
+        eventDescription: event.description
+      });
+    } else {
+      this.isNewEvent = true;
+      this.eventForm = this.formBuilder.group({
+        event: [''],
+        eventTitle: [''],
+        eventLocation: [''],
+        eventDate: [''],
+        eventDescription: ['']
+      });
+    }
+  }
 }
+
+export interface Element {
+  id: number;
+  name: string;
+  country: Country;
+}
+
+export interface Country {
+  id: number;
+  name: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {id: 0, name: 'Italo Costa Martins', country: {id: 0, name: 'Brazil'}},
+  {id: 1, name: 'Nizami Zamanov', country: {id: 1, name: 'Arzebajan'}},
+  {id: 2, name: 'Lorant Vrinceanu', country: {id: 2, name: 'Moldova'}},
+  {id: 3, name: 'Oleksi IDontCare', country: {id: 3, name: 'Oleksiland'}},
+  {id: 4, name: 'Saaker Whatever', country: {id: 4, name: 'Saakerland'}},
+  {id: 5, name: 'Leutlt Nope', country: {id: 5, name: 'Nopeland'}},
+  {id: 6, name: 'Dummy Dumber', country: {id: 6, name: 'Dumbland'}},
+]
